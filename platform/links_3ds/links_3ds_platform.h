025d00d94ad18307605e2d97584a58fcf0ce7cb0
Azimn <jratican@gmail.com>
2026-08-02 09:56:48 -0500

    Define neutral Links 3DS adapter interface

---
+++ platform/links_3ds/links_3ds_platform.h
@@ -0,0 +1,47 @@
+#ifndef LINKS_3DS_PLATFORM_H
+#define LINKS_3DS_PLATFORM_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "../../source/gfx_3ds.h"
+
+typedef enum links_3ds_key {
+    LINKS_3DS_KEY_NONE = 0,
+    LINKS_3DS_KEY_UP,
+    LINKS_3DS_KEY_DOWN,
+    LINKS_3DS_KEY_LEFT,
+    LINKS_3DS_KEY_RIGHT,
+    LINKS_3DS_KEY_ENTER,
+    LINKS_3DS_KEY_ESCAPE,
+    LINKS_3DS_KEY_BACK,
+    LINKS_3DS_KEY_RELOAD,
+    LINKS_3DS_KEY_PAGE_UP,
+    LINKS_3DS_KEY_PAGE_DOWN,
+    LINKS_3DS_KEY_CLOSE
+} links_3ds_key_t;
+
+typedef enum links_3ds_pointer_action {
+    LINKS_3DS_POINTER_MOVE = 0,
+    LINKS_3DS_POINTER_DOWN,
+    LINKS_3DS_POINTER_UP,
+    LINKS_3DS_POINTER_DRAG
+} links_3ds_pointer_action_t;
+
+typedef struct links_3ds_event_sink {
+    void *context;
+    void (*keyboard)(void *context, links_3ds_key_t key, uint32_t modifiers);
+    void (*pointer)(void *context,
+                    int x,
+                    int y,
+                    links_3ds_pointer_action_t action,
+                    uint32_t buttons);
+} links_3ds_event_sink_t;
+
+bool links_3ds_platform_init(void);
+void links_3ds_platform_shutdown(void);
+void links_3ds_platform_set_event_sink(const links_3ds_event_sink_t *sink);
+void links_3ds_platform_poll(void);
+void links_3ds_platform_timer_callback(void *unused);
+
+#endif
